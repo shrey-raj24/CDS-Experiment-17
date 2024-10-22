2f3833b763c06d7f94f17bc9f7c2c74012224dd7
Shrey raj <shreyraj37@gmail.com>
2024-10-23 02:33:13 +0530

    Add files via upload

---
+++ Shrey_B2_123_Exp_17A_CDS.cpp
@@ -0,0 +1,48 @@
+// NAME - SHREY RAJ
+// PRN - 23070123123
+// EXPERIMENT - 17(A)
+
+// LINKED LIST 
+
+#include<iostream>
+using namespace std; 
+ 
+ class Link {
+    public:
+    int data;
+    Link* next;
+    Link(int num) {
+        data=num;
+        next=NULL;
+    }
+ };
+ void insert_head(Link* &head, int data) {
+    Link* new_node=new Link(data);
+    new_node->next = head; 
+    head = new_node;
+ }
+ void disp(Link* head) {
+    Link* temp = head;
+    while(temp!=NULL) { 
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    } 
+    cout<<"NULL"<<endl;
+ }
+
+ int main() {
+    Link* head = NULL;
+    insert_head(head, 30);
+    disp(head);
+    insert_head(head, 32);
+    disp(head);
+    insert_head(head, 35);
+    disp(head);
+    //l1.disp(); 
+ }  
+
+// Output 
+
+// 30->NULL
+// 32->30->NULL
+// 35->32->30->NULL
